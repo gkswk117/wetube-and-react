04657572f268d353423c5bd246bc3e3667a7a213
Hanwoong <gkswk117@gmail.com>
2022-11-09 13:16:34 +0900

    [풀스택 유튜브 클론코딩] #3.4까지 완료.

--- src/server.js
+++ src/server.js
@@ -2,4 +2,10 @@ import express from "express"
 const PORT = 4000
 const app = express()
 
+function handleHome (req, res) {
+    console.log("Somebody is tring to go home.")
+    return res.send("I still love you.");
+}
+app.get("/", handleHome)
+
 app.listen(PORT, ()=>{console.log(`Server listening on port http://localhost:${PORT}`)})
