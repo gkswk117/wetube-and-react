14f485fe6411150ca1ce44353225043576b82570
gkswk117 <gkswk117@gmail.com>
2022-11-10 13:21:24 +0900

    [풀스택 유튜브 클론코딩] #3.6까지 완료.

--- src/server.js
+++ src/server.js
@@ -2,10 +2,26 @@ import express from "express"
 const PORT = 4000
 const app = express()
 
-function handleHome (req, res) {
-    console.log("Somebody is tring to go home.")
-    return res.send("I still love you.");
+const logger=(req,res,next)=>{
+    console.log(`Someone is going to "${req.url}" with method {${req.method}}`)
+    next();
 }
+const privateMiddleware = (req, res, next)=>{
+    const url = req.url
+    if(url ==="/protected"){
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    next();
+}
+function handleHome (req, res) {
+    return res.send("I love middlewares.");
+}
+function handleProtected(req,res){
+    console.log("이건 실행되지 않음.")
+}
+app.use(logger)
+app.use(privateMiddleware)
 app.get("/", handleHome)
+app.get("/protected", handleProtected)
 
 app.listen(PORT, ()=>{console.log(`Server listening on port http://localhost:${PORT}`)})
