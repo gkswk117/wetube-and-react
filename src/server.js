177faf90b7b420329c40aab822e48b7c4bbe9b0c
gkswk117 <gkswk117@gmail.com>
2022-11-11 12:56:15 +0900

    [풀스택 유튜브 클론코딩] #3 Introduction to express 완료.

--- src/server.js
+++ src/server.js
@@ -1,27 +1,20 @@
 import express from "express"
+import morgan from "morgan"
+
 const PORT = 4000
 const app = express()
 
-const logger=(req,res,next)=>{
+const loggerMiddleware=(req,res,next)=>{
     console.log(`Someone is going to "${req.url}" with method {${req.method}}`)
-    next();
-}
-const privateMiddleware = (req, res, next)=>{
-    const url = req.url
-    if(url ==="/protected"){
-        return res.send("<h1>Not Allowed</h1>")
-    }
     next();
 }
 function handleHome (req, res) {
     return res.send("I love middlewares.");
 }
-function handleProtected(req,res){
-    console.log("이건 실행되지 않음.")
-}
-app.use(logger)
-app.use(privateMiddleware)
+app.use(morgan("dev"))
+//middleware를 npm에서 설치해서 사용해봄.
+app.use(loggerMiddleware)
+//middleware를 직접 만들어서 사용해봄.
 app.get("/", handleHome)
-app.get("/protected", handleProtected)
 
 app.listen(PORT, ()=>{console.log(`Server listening on port http://localhost:${PORT}`)})
