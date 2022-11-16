3bbdedb779a0d0bcaeb46afc9ca525521b08c14c
Hanwoong <gkswk117@gmail.com>
2022-11-16 12:22:02 +0900

    [풀스택 유튜브 클론코딩] #4 Routers 완료.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,9 +2,9 @@ import express from "express"
 import {watchVideo, editVideo, deleteVideo, uploadVideo} from "../controllers/videoController"
 
 const videoRouter = express.Router()
+videoRouter.get("/:id(\\d+)", watchVideo)
 videoRouter.get("/upload", uploadVideo)
-videoRouter.get("/:id", watchVideo)
-videoRouter.get("/:id/edit", editVideo)
-videoRouter.get("/:id/delete", deleteVideo)
+videoRouter.get("/:id(\\d+)/edit", editVideo)
+videoRouter.get("/:id(\\d+)/delete", deleteVideo)
 
 export default videoRouter
