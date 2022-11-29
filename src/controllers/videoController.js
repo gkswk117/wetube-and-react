54dada0535bac7457a8a87cb94977c93670a8a72
Hanwoong <gkswk117@gmail.com>
2022-11-29 14:36:45 +0900

    [풀스택 유튜브 클론코딩] #6.21까지 완료.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,32 +5,6 @@ const fakeUser = {
     username: "gkswk",
     loggedIn: false
 }
-const videos = [
-    {
-        title:"First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views:59,
-        id:1,
-    },
-    {
-        title:"Second Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views:1,
-        id:2,
-    },
-    {
-        title:"Third Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views:1000,
-        id:3,
-    }
-]
 //controller
 //Using callback function
 // let testVar = {t:"t"}
@@ -58,34 +32,8 @@ export const home = async(req, res)=>{
         return res.send("error!!")
     }
 }
-
-
-export const searchVideo = (req, res) => {
-    console.log("req.params는")
-    console.log(req.params)
-    res.render("search", {pageTitle: "Search", fakeUser:fakeUser})
-}
-
-export const watchVideo = (req, res) => {
-    const video = videos[req.params.id-1]
-    res.render("watch", {pageTitle: "Watch", video: video, fakeUser:fakeUser})
-}
-export const getEdit = (req, res) => {
-    const video = videos[req.params.id-1]
-    return res.render("edit", {pageTitle:`Editing ${video.title}`, video:video, fakeUser:fakeUser})
-}
-export const postEdit = (req, res)=>{
-    const id = req.params.id
-    console.log("req.body는")
-    console.log(req.body)
-    videos[id-1].title = req.body.potato
-    //가짜 데이터베이스에 정보 입력
-    //가짜 데이터베이스는 서버의 메모리에만 존재하니까 서버를 재시작하게 되면 원래 코드에 적혀있던 정보로 되돌아오게 됨.
-    //이제 #6.7부터 진짜 데이터베이스(MongoDB)에 정보를 입력하고 저장하는 방법을 배울거다.
-    return res.redirect(`/video/${id}`)
-}
-//연습용 코드
 export const getUpload = (req, res) =>{
+    const {id} = req.params
     return res.render("upload", {pageTitle:`Upload`, fakeUser:fakeUser} )
 }
 export const postUpload = async (req, res) =>{
@@ -100,5 +48,36 @@ export const postUpload = async (req, res) =>{
     }catch(err){
         console.log(err)
         return res.render("upload", {pageTitle:`Upload`, fakeUser:fakeUser, errorMessage:err._message} )
-    }   
+    }
+}
+export const watchVideo = async (req, res) => {
+    const video = await Video.findById(req.params.id)
+    if(!video){
+        return res.render("404", {fakeUser:fakeUser})
+    }
+    return res.render("watch", {pageTitle: video.title, video: video, fakeUser:fakeUser})
+}
+export const getEdit = async(req, res) => {
+    const video = await Video.findById(req.params.id)
+    if(!video){
+        return res.render("404", {fakeUser:fakeUser})
+    }
+    return res.render("edit", {pageTitle:`Editing ${video.title}`, video:video, fakeUser:fakeUser})
+}
+export const postEdit = async(req, res)=>{
+    const video = await Video.findById(req.params.id)
+    if(!video){
+        return res.render("404", {fakeUser:fakeUser})
+    }
+    video.title = req.body.title
+    video.hashtags = req.body.hashtags.split(",").map(word => word.startsWith('#')? word :`#${word}`)
+    await video.save()
+    return res.redirect(`/video/${req.params.id}`)
+}
+
+
+export const searchVideo = (req, res) => {
+    console.log("req.params는")
+    console.log(req.params)
+    res.render("search", {pageTitle: "Search", fakeUser:fakeUser})
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,10 +2,9 @@ import express from "express"
 import {watchVideo, getEdit, postEdit, getUpload, postUpload} from "../controllers/videoController"
 
 const videoRouter = express.Router()
-videoRouter.route("/:id(\\d+)").get(watchVideo)
+videoRouter.route("/:id([0-9a-f]{24})").get(watchVideo)
 //videoRouter.get("/:id(\\d+)", watchVideo) 과 같음.
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit)
-videoRouter.route("/upload").get(getUpload)
-videoRouter.route("/postTestkkk").post(postUpload)
-
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit)
+videoRouter.route("/upload").get(getUpload).post(postUpload)
+videoRouter.route("/edit").get(getEdit).post(postEdit)
 export default videoRouter
