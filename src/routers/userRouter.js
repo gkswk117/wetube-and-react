d8c1494a7cdef690c872c01233ffcbc864e2e7e6
Hanwoong <gkswk117@gmail.com>
2022-12-11 10:28:26 +0900

    .

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,7 +2,7 @@ import express from "express"
 import {getEdit, postEdit, deleteUser, seeUser,logout} from "../controllers/userController"
 const userRouter = express.Router()
 
-userRouter.get("/:id", seeUser)
+//userRouter.get("/:id", seeUser)
 userRouter.get("/logout", logout)
 userRouter.route("/edit").get(getEdit).post(postEdit)
 userRouter.get("/delete", deleteUser)
