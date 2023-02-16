4362c3f80667dd2c5f99ffb1ebe2e6d08414a579
gkswk117 <gkswk117@gmail.com>
2023-02-16 12:28:49 +0900

    [풀스택 유튜브 클론코딩] #11.1 Play Pause 완료.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,36 @@
-console.log("It's videoPlayer js file")
+const video = document.querySelector("video")
+const playBtn = document.getElementById("play")
+const muteBtn = document.getElementById("mute")
+const time = document.getElementById("time")
+const volume = document.getElementById("volume")
+
+const handlePlayClick = () =>{
+    // if the video is playing, pause it.
+    // else play the video.
+    if(video.paused){
+        video.play()
+    }else{
+        video.pause()
+    }
+}
+
+const handlePause = ()=>{
+    playBtn.innerText = "Play"
+}
+
+const handlePlay =()=>{
+    playBtn.innerText = "Pause"
+}
+
+const handleMute = () =>{
+    if(video.paused){
+        video.play()
+    }else{
+        video.pause()
+    }
+}
+
+playBtn.addEventListener("click",handlePlayClick)
+muteBtn.addEventListener("click", handleMute)
+video.addEventListener("pause", handlePause)
+video.addEventListener("play", handlePlay)
