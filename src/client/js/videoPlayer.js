0caeba665f59c672ece10e8f8a88e20bb209d2df
gkswk117 <gkswk117@gmail.com>
2023-02-21 19:41:13 +0900

    [풀스택 유튜브 클론코딩] #11.2 Mute and Unmute 완료. [풀스택 유튜브 클론코딩] #11.3 Volume 완료.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,35 +2,50 @@ const video = document.querySelector("video")
 const playBtn = document.getElementById("play")
 const muteBtn = document.getElementById("mute")
 const time = document.getElementById("time")
-const volume = document.getElementById("volume")
+const volumeInput = document.getElementById("volume")
 
+video.volume = localStorage.getItem("volume")
+volumeInput.value = video.volume
+video.muted = localStorage.getItem("mute")==="true" ? true : false
+muteBtn.innerText = video.muted ? "Unmute" : "Mute"
+if(video.muted){
+    volumeInput.value = 0   
+}
 const handlePlayClick = () =>{
-    // if the video is playing, pause it.
-    // else play the video.
     if(video.paused){
         video.play()
     }else{
         video.pause()
     }
+    playBtn.innerText = video.paused ? "Play" : "Pause"
 }
-
-const handlePause = ()=>{
-    playBtn.innerText = "Play"
-}
-
-const handlePlay =()=>{
-    playBtn.innerText = "Pause"
-}
-
-const handleMute = () =>{
-    if(video.paused){
-        video.play()
+const handleMuteClick = () =>{
+    video.muted = video.muted ? false : true
+    muteBtn.innerText = video.muted ? "Unmute":"Mute"
+    if(video.muted){
+        volumeInput.value = 0   
     }else{
-        video.pause()
+        if(video.volume===0){
+            video.volume = 0.1
+        }
+        volumeInput.value = video.volume
     }
+    localStorage.setItem("volume", video.volume)
+    localStorage.setItem("mute", video.muted)
 }
-
-playBtn.addEventListener("click",handlePlayClick)
-muteBtn.addEventListener("click", handleMute)
-video.addEventListener("pause", handlePause)
-video.addEventListener("play", handlePlay)
+const handleVolumeChange = (event) => {
+    video.volume= event.target.value
+    if(video.muted){
+        video.muted = false
+        muteBtn.innerText = "Mute"
+    }
+    if(video.volume===0){
+        video.muted = true
+        muteBtn.innerText = "Unmute"
+    }
+    localStorage.setItem("volume", video.volume)
+    localStorage.setItem("mute", video.muted)
+}
+playBtn.addEventListener("click", handlePlayClick)
+muteBtn.addEventListener("click", handleMuteClick)
+volumeInput.addEventListener("input", handleVolumeChange)
