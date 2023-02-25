d28d62b9e7fa7abfa720e3acb80c52c2ba375aec
Hanwoong <gkswk117@gmail.com>
2023-02-26 02:42:46 +0900

    [풀스택 유튜브 클론코딩] #11.4 Duraation and Current Time 완료.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector("video")
 const playBtn = document.getElementById("play")
 const muteBtn = document.getElementById("mute")
-const time = document.getElementById("time")
 const volumeInput = document.getElementById("volume")
+const currentTime = document.getElementById("currentTime")
+const totalTime = document.getElementById("totalTime")
 
 video.volume = localStorage.getItem("volume")
 volumeInput.value = video.volume
@@ -46,6 +47,14 @@ const handleVolumeChange = (event) => {
     localStorage.setItem("volume", video.volume)
     localStorage.setItem("mute", video.muted)
 }
+const handleLoadedMetadata = () => {
+    totalTime.innerText = Math.floor(video.duration)
+}
+const handleTimeUpdate = () => {
+    currentTime.innerText = Math.floor(video.currentTime)
+}
 playBtn.addEventListener("click", handlePlayClick)
 muteBtn.addEventListener("click", handleMuteClick)
 volumeInput.addEventListener("input", handleVolumeChange)
+video.addEventListener("loadedmetadata", handleLoadedMetadata)
+video.addEventListener("timeupdate", handleTimeUpdate);
