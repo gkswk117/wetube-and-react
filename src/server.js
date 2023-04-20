2985dbfd4ac7d26760007b2984d409e508512c38
Hanwoong <gkswk117@gmail.com>
2023-04-20 19:57:26 +0900

    [풀스택 유튜브 클론코딩] #7.15 Environment Variables (07:22) 완료.

--- src/server.js
+++ src/server.js
@@ -18,10 +18,10 @@ app.use(loggerMiddleware)
 app.use(express.urlencoded({extended:true}))
 //
 app.use(session({
-    secret:"Hello!",
+    secret:process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({mongoUrl:"mongodb://127.0.0.1:27017/wetube"})
+    store: MongoStore.create({mongoUrl:process.env.DB_URL})
 }))
 // Session 연습
 // app.use((req, res, next) => {
