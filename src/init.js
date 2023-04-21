ba5d2decc36fadcd692b9af808254f747008ba8e
gkswk117 <gkswk117@gmail.com>
2023-04-21 17:46:43 +0900

    배포하기 전에 const PORT = process.env.PORT || 4000 로 수정해줘야함. 내가 따로 Environment variables에 적지 않았어도 Cloudtype에서 ​PORT변수를 정해서 넘겨준다.

--- src/init.js
+++ src/init.js
@@ -4,6 +4,6 @@ import "./db"
 import "./models/Video"
 import "./models/User"
 import app from "./server"
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, ()=>{console.log(`Server listening on port http://localhost:${PORT}`)})
